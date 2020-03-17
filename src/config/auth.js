7cee581aede78ef199e1ef67852aff6748e09705
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-03-17 15:51:51 -0300

    Added: session controller, User Model, authentication configuration, data validation on entry and generation of a web token

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secretHash: '95a20ec124be28749016ebc1e46382e8',
+  expirationTime: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello World');
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
