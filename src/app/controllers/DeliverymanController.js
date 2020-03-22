7c051b1a4c77dcb55c1f0e976af75420b11610de
Dheimison <dheimisonlo@gmail.com>
2020-03-22 00:04:07 -0300

    feat: creates a controller that handles the files table and deliverymen

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,114 @@
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+
+class DeliverymanController {
+  async index(req, res) {
+    const deliverymen = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+    });
+    return res.status(200).json(deliverymen);
+  }
+
+  async store(req, res) {
+    const { name, email } = req.query;
+
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { email },
+    });
+
+    if (deliverymanExists) {
+      return res.status(400).json({ error: 'Delivery man already exists.' });
+    }
+
+    if (req.file) {
+      const { originalname, filename: path } = req.file;
+
+      const file = await File.create({
+        name: originalname,
+        path,
+      });
+
+      const deliveryman = await Deliveryman.create({
+        name,
+        email,
+        avatar_id: file.id,
+      });
+
+      return res.status(200).json({ deliveryman });
+    }
+
+    const deliveryman = await Deliveryman.create({
+      name,
+      email,
+    });
+
+    return res.json({ deliveryman });
+  }
+
+  async update(req, res) {
+    const { email } = req.query;
+
+    const deliveryman = await Deliveryman.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!deliveryman) {
+      return res.status(404).json({ error: 'Delivery man does not exists' });
+    }
+
+    if (email) {
+      const deliverymanExists = await Deliveryman.findOne({
+        where: { email },
+      });
+
+      if (deliverymanExists) {
+        return res.status(401).json({ error: 'Delivery man already exists.' });
+      }
+    }
+
+    if (req.file) {
+      const { originalname, filename: path } = req.file;
+
+      const avatar = await File.findOne({
+        where: { id: deliveryman.avatar_id },
+      });
+
+      if (avatar) {
+        const { url } = await avatar.update({
+          name: originalname,
+          path,
+        });
+
+        const { id, name } = await deliveryman.update(req.query);
+
+        return res.status(200).json({ id, name, email, url });
+      }
+
+      const file = await File.create({
+        name: originalname,
+        path,
+      });
+
+      const { id, name } = await deliveryman.update({
+        name: req.query.name,
+        email,
+        avatar_id: file.id,
+      });
+
+      return res.status(200).json({ id, name, email, url: file.url });
+    }
+
+    const { id, name } = await deliveryman.update(req.query);
+
+    return res.status(200).json({ id, name, email });
+  }
+}
+
+export default new DeliverymanController();
