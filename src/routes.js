dd325a2aa74acb192260fa44242aa01a51e8b4f6
Dheimison <dheimisonlo@gmail.com>
2020-03-22 00:10:27 -0300

    chore: delete useless files and add new setting

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -13,5 +17,13 @@ routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients', RecipientController.update);
+
+routes.get('/deliverymen', DeliverymanController.index);
+routes.post('/deliverymen', upload.single('file'), DeliverymanController.store);
+routes.put(
+  '/deliverymen/:id',
+  upload.single('file'),
+  DeliverymanController.update
+);
 
 export default routes;
