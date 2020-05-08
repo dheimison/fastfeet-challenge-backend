e2699cfafab6ba854174cb32f942c12884e8123c
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-05-07 22:43:32 -0300

    fix: url that was being returned without the "http://"

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `${process.env.APP_URL}/files/${this.path}`;
+            return `http://${process.env.APP_URL}/files/${this.path}`;
           },
         },
       },
